61d30bb8e67d90ca0b08d1ccfce45523d2daf7af
Kozak24 <okozak24@gmail.com>
2017-11-26 20:15:37 +0200

    Add Firmata protocol

--- Modified/Modified.ino
+++ Modified/Modified.ino
@@ -1,20 +1,44 @@
 #include "Tensor.h"
+#include "IMU.h"
+
 Tensor tensor1(0, "G");
 Tensor tensor2(1, "H");
+Tensor tensor3(2, "I");
+Tensor tensor4(3, "J");
+
+IMU imu;
+Acc acc;
+
+boolean sending = false;
 
 void setup()
 {
   Serial.begin(57600);
 }
 
-void loop()
-{
-  //tensor2.tensorRead();
-  Serial.print(tensor1.tensorRead());
-  //Serial.print(analogRead(0));
-  Serial.print("\t"); 
+void loop() {
 
-  Serial.println(tensor2.tensorRead());
-  //Serial.println(analogRead(1));
+  String string = acc.readAcc(); // return something like this A099B001C-15 as string
+  string += imu.readGyro(); // return something like this D0179E-179F0025 as string
+  string += tensor1.read(); // return something like this G01 as string
+  string += tensor2.read(); // return something like this H99 as string
+  string += tensor3.read(); // return something like this I38 as string
+  string += tensor4.read(); // return something like this J65 as string
+  string += "K";
+
+
+if (sending) {
+    Serial.print(string);
+    sending = false;
+  }
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    if (inChar == 'N') {
+      sending = true;
+    }
+  }
 }
 
